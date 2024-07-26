f24d42f4aeae4a6a3ad2febde11a0677bd958e52
bhanukumari <117707378+bhanukumari@users.noreply.github.com>
2024-07-26 12:06:57 +0530

    Update bucketls.sh

--- bucketls.sh
+++ bucketls.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 BUCKET_NAME="bp-23"
-# Set this to the folder path like this("folder/subfolder/")
 PREFIX=""
 # List the contents of the bucket
 echo "Listing contents of bucket: $BUCKET_NAME"
 
 # If you have a prefix, include it in the command
 if [ -n "$PREFIX" ]; then
-  aws s3 ls s3://$BUCKET_NAME/$PREFIX --recursive
+  aws s3 ls s3://$BUCKET_NAME/$PREFIX --recursive | awk '{print $4}'
 else
- # If PREFIX is not set, list all contents in the bucket
-  aws s3 ls s3://$BUCKET_NAME --recursive
+  # If PREFIX is not set, list all contents in the bucket
+  aws s3 ls s3://$BUCKET_NAME --recursive | awk '{print $4}'
 fi
